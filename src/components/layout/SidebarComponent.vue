07a1d069aec352fc9fbe4242c523d1f4dc0a6a9c
JohnXin123 <heinthuralin123@gmail.com>
2023-05-30 09:30:24 +0630

    CORS fix and Dashboard link added

--- src/components/layout/SidebarComponent.vue
+++ src/components/layout/SidebarComponent.vue
@@ -2,12 +2,14 @@
   <div class="sidebar bg-white rounded-2xl p-6 drop-shadow-sm ring-1 ring-slate-300 overflow-hidden">
     <div class="divide-y">
       <div class="flex gap-3 flex-col py-2">
-          <div class="flex items-center gap-2 cursor-pointer py-1">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-500 h-6 max-[768px]:mx-auto">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-            </svg>
-            <span class="text-slate-900 text-lg max-[1024px]:text-sm max-[768px]:hidden">Dashboard</span>
-          </div>
+          <router-link :to="{ name: 'dashboard' }">
+            <div class="flex items-center gap-2 cursor-pointer py-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 text-slate-500 h-6 max-[768px]:mx-auto">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              </svg>
+              <span class="text-slate-900 text-lg max-[1024px]:text-sm max-[768px]:hidden">Dashboard</span>
+            </div>
+          </router-link>
         
           <router-link :to="{ name: 'users' }">
             <div class="flex items-center gap-2 cursor-pointer py-1"> 
